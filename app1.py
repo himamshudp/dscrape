ff8fd89fe652f282423a7d0088c7664ad6d01726
Himanshu Sharma <69843278+himamshudp@users.noreply.github.com>
2020-11-19 13:40:11 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,7 +1,9 @@
 import bs4
+# import requests
+# import shutil
 import time
 import os
-import json
+#import json
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
@@ -81,7 +83,7 @@ def dmart():
     df_copy['weight'] = df_copy['product_name'].str.split(': ').str[1]
     df_copy['weight'] = df_copy['product_name'].str.split(' : ').str[1]
     df_copy['weight'] = df_copy['product_name'].str.split(':').str[1]
-    df_copy['app_Weight'] = df_copy['weight'].str.split('').str[1]
+    #df_copy['app_Weight'] = df_copy['weight'].str.split('').str[1]
     df_copy['app_Weight'] = df_copy['weight'].str.split(' ').str[1]
     df_copy['app_Weight'] = df_copy['app_Weight'].replace({'x': '*'}, regex=True)
     # create scale col from product name col
